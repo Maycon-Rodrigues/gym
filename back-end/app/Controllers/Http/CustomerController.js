e76d3959ab1a908f8639fc0c912f45541d00b547
Maycon <mrtdb14@hotmail.com>
2019-01-25 15:08:40 -0200

    Add association on method show and make validation

--- back-end/app/Controllers/Http/CustomerController.js
+++ back-end/app/Controllers/Http/CustomerController.js
@@ -6,49 +6,60 @@ class CustomerController {
   /**
    * Show a list of all customers.
    * GET customers
-  */
-  async index () {
+   */
+  async index() {
     const customers = Customer.all()
 
     return customers
   }
 
-
   /**
    * Create/save a new customer.
    * POST customers
-  */
-  async store ({ request }) {
-      const data = request.only(['name', 'born'])
-      const customer = await Customer.create(data)
+   */
+  async store({ request }) {
+    const data = request.only(['name', 'born'])
+    const customer = await Customer.create(data)
 
-      return customer
+    return customer
   }
 
   /**
    * Display a single customer.
    * GET customers/:id
    */
-  async show ({ params }) {
-    const customer = await Customer.findOrFail(params.id)
+  async show({ params, response }) {
+    const id = await Customer.find(params.id)
+    const customer = await Customer.query()
+      .where('id', params.id)
+      .with('evaluations')
+      .fetch()
 
-    return customer
+    if (!id) {
+      response.status(404).send('Customer not found')
+    } else {
+      return customer
+    }
   }
 
   /**
    * Update customer details.
    * PUT or PATCH customers/:id
-  */
-  async update ({ params, request }) {
-    const customer = await Customer.findOrFail(params.id)
-    const data = request.only(['name', 'born'])
+   */
+  async update({ params, request, response }) {
+    const customer = await Customer.find(params.id)
 
-    customer.merge(data)
-    await customer.save()
-    
-    return customer
+    if (!customer) {
+      response.status(404).send('Customer not found')
+    } else {
+      const data = request.only(['name', 'born'])
+
+      customer.merge(data)
+      await customer.save()
+
+      return customer
+    }
   }
-
 }
 
 module.exports = CustomerController
