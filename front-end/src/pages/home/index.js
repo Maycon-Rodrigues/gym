d3922e76b7db7d4bd063636e8908662e53322fd2
Maycon <mrtdb14@hotmail.com>
2019-03-01 16:58:20 -0300

    Add material-ui

--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
+import AppBar from '../../component/AppBar';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 class Home extends Component {
   render() {
-    return <h1>HOME</h1>;
+    return (
+      <div>
+        <AppBar />
+        <Grid container direction='row' justify='center' alignItems='center'>
+          <Typography style={{ paddingTop: 20 }} variant='h4'>
+            Home
+          </Typography>
+        </Grid>
+      </div>
+    );
   }
 }
 
