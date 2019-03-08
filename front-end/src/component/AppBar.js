baec589d8dd63701cba4628b9a5db9cefe77c83c
Maycon <mrtdb14@hotmail.com>
2019-03-08 16:13:08 -0300

    Change app bar fo using persistent drawer

--- front-end/src/component/AppBar.js
+++ front-end/src/component/AppBar.js
@@ -1,117 +1,203 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import { isAuthenticated } from '../services/auth';
-import AvatarToggle from './AvatarToggle';
 
 import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
-import Drawer from 'material-ui/Drawer';
-import Divider from '@material-ui/core/Divider';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import { blue300 } from 'material-ui/styles/colors';
 import Toolbar from '@material-ui/core/Toolbar';
+import AvatarToggle from './AvatarToggle';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import { blue300 } from 'material-ui/styles/colors';
 
-const styles = {
+const drawerWidth = 240;
+
+const styles = theme => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
   },
-  bar: {
-    background: blue300,
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
-    marginLeft: -12,
+    marginLeft: 12,
     marginRight: 20,
   },
-  appName: {
-    padding: 10,
-    marginLeft: 10,
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
   left: {
+    paddingRight: 20,
     marginLeft: 'auto',
   },
-};
+});
 
-class SimpleAppBar extends Component {
+class PersistentDrawerLeft extends React.Component {
   state = {
-    drawerOpened: false,
+    open: false,
   };
 
-  handleDrawer = () => {
-    this.setState({ drawerOpened: !this.state.drawerOpened });
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
+    const { open } = this.state;
 
     return (
       <div className={classes.root}>
-        <AppBar className={classes.bar} position='static'>
-          <Drawer
-            docked={false}
-            open={this.state.drawerOpened}
-            onRequestChange={this.handleDrawer}
-          >
-            <MenuList>
-              <Typography className={classes.appName} variant='h6'>
-                Meu App
-              </Typography>
-              <Divider />
-              <MenuItem style={{ background: '#eee' }} component={Link} to='/'>
-                Painel
-              </MenuItem>
-              <Divider />
-              <MenuItem component={Link} to='/customers'>
-                Clientes
-              </MenuItem>
-              <MenuItem component={Link} to='/evaluations'>
-                Avaliações
-              </MenuItem>
-            </MenuList>
-          </Drawer>
-          <Toolbar>
+        <CssBaseline />
+        <AppBar
+          position='static'
+          style={{ background: blue300 }}
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar disableGutters={!open}>
             {isAuthenticated() ? (
-              <IconButton
-                className={classes.menuButton}
-                color='inherit'
-                onClick={this.handleDrawer}
-              >
-                <MenuIcon />
-              </IconButton>
+              <>
+                <IconButton
+                  color='inherit'
+                  aria-label='Open drawer'
+                  onClick={this.handleDrawerOpen}
+                  className={classNames(
+                    classes.menuButton,
+                    open && classes.hide
+                  )}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant='h6' color='inherit' noWrap>
+                  App name
+                </Typography>
+                <div className={classes.left}>
+                  <AvatarToggle />
+                </div>
+              </>
             ) : (
-              <Typography variant='h6' color='inherit'>
-                Nome do App
-              </Typography>
-            )}
-            {isAuthenticated() ? (
-              <div className={classes.left}>
-                <AvatarToggle />
-              </div>
-            ) : (
-              <Button
-                component={Link}
-                to='/signin'
-                className={classes.left}
-                color='inherit'
-              >
-                Login
-              </Button>
+              <>
+                <Typography
+                  style={{ paddingLeft: 20 }}
+                  variant='h6'
+                  color='inherit'
+                  noWrap
+                >
+                  App name
+                </Typography>
+                <Button
+                  component={Link}
+                  to='/signin'
+                  className={classes.left}
+                  color='inherit'
+                >
+                  Login
+                </Button>
+              </>
             )}
           </Toolbar>
         </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant='persistent'
+          anchor='left'
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'ltr' ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <MenuList>
+            <Divider />
+            <MenuItem style={{ background: '#eee' }} component={Link} to='/'>
+              <InboxIcon style={{ paddingRight: 40 }} />
+              Painel
+            </MenuItem>
+            <Divider />
+            <MenuItem component={Link} to='/customers'>
+              Clientes
+            </MenuItem>
+            <MenuItem component={Link} to='/evaluations'>
+              Avaliações
+            </MenuItem>
+          </MenuList>
+        </Drawer>
       </div>
     );
   }
 }
 
-SimpleAppBar.propTypes = {
+PersistentDrawerLeft.propTypes = {
   classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleAppBar);
+export default withStyles(styles, { withTheme: true })(PersistentDrawerLeft);
